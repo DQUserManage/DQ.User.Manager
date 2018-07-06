167263d10f4ade187ddb5a2cda035d32ab8a46a1
ShengFeiChen <476330769@qq.com>
2018-07-06 10:30:04 +0800

    Revert "忽略"
    This reverts commit 2f5d7e86641cfcd48cd344b8b38f69e2a5221c3a.

--- DQ.User.Test/DQ.User.TestDlg.cpp
+++ DQ.User.Test/DQ.User.TestDlg.cpp
@@ -105,7 +105,7 @@ BOOL CDQUserTestDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);	
 
 	//
-	CString csConn = _T("(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=127.0.0.1)(PORT=1521))(CONNECT_DATA=(SID= ORCL)));User Id=OVERDRIVEBACKUP;Password=123456");
+	CString csConn = _T("(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=127.0.0.1)(PORT=1521))(CONNECT_DATA=(SID= ORCL)));User Id=perf;Password=perf");
 	CUserManagerDataService::GetInstance()->DBInit(csConn, (int)DB_Oracle);
 
 	return TRUE; 
